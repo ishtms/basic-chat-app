fd5af4c0b682cb5a275454959d44580653a79372
codemodeco <codemode.co@gmail.com>
2017-10-24 18:26:33 +0530

    changed chat app to countdown timer

--- public/js/customjs.js
+++ public/js/customjs.js
@@ -340,7 +340,7 @@
 
 	    // Time calculations for days, hours, minutes and seconds
 
-	    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	    var days = Math.floor(distance / (24 * 60 * 60 * 24));
 
 	    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 
